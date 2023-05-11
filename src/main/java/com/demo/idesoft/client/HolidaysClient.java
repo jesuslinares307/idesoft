448071d781d377b956c29ee6043d2f72fbb30c3a
Jesus Linares <jesuslinares307@gmail.com>
2023-05-11 12:51:57 -0300

    se modifico el cliente, para solventar un error a la hora de dockerizar el proyecto, no estaba teniendo salida y era necesario agregar los headers

--- src/main/java/com/demo/idesoft/client/HolidaysClient.java
+++ src/main/java/com/demo/idesoft/client/HolidaysClient.java
@@ -4,10 +4,16 @@ import com.demo.idesoft.dto.HolidayResponse;
 import com.demo.idesoft.model.Holiday;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpEntity;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -17,15 +23,18 @@ public class HolidaysClient {
     private static final Logger log = LoggerFactory.getLogger(HolidaysClient.class);
 
     public HolidaysClient() {
+
         this.restTemplate = new RestTemplate();
     }
 
     public List<Holiday> getHolidayData() {
+        HttpEntity<String> entity = getStringHttpEntity();
+
         try {
             log.info("[getHolidayData]: IN");
             //TODO generar en el applications properties
             String url = "https://api.victorsanmartin.com/feriados/en.json";
-            HolidayResponse holidayResponse = restTemplate.getForObject(url,HolidayResponse.class);
+            HolidayResponse holidayResponse = restTemplate.exchange(url, HttpMethod.GET, entity, HolidayResponse.class).getBody();
 
             log.info("Response: {}", holidayResponse.toString());
 
@@ -37,5 +46,14 @@ public class HolidaysClient {
             log.error("Error al realizar la solicitud HTTP: {}", e.getMessage());
             throw new RuntimeException("Error al realizar solicitud");
         }
+    }
+
+    private static HttpEntity<String> getStringHttpEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("user-agent", "Application");
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        return entity;
     }
 }
